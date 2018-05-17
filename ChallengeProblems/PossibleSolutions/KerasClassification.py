62d6283461160ab697309a908ca19dcefd123d81
Gregory Teichert <gregteichert@0587379922.wireless.umich.net>
2018-05-17 14:38:21 -0400

    Initial examples.

---
+++ ChallengeProblems/PossibleSolutions/KerasClassification.py
@@ -0,0 +1,40 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+import numpy as np
+
+# Create model
+model = Sequential()
+model.add(Dense(units=1000, activation='relu', input_dim=4))
+model.add(Dropout(0.5))
+model.add(Dense(units=500, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=70, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=3, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=keras.optimizers.Adagrad(lr=0.01),
+              metrics=['accuracy'])
+
+# Import data
+dataIn = np.genfromtxt('iris_training.csv',delimiter=',',skip_header=1)
+x_train = dataIn[:,0:-1]
+y_train = keras.utils.to_categorical(dataIn[:,-1], num_classes=3)
+
+dataIn = np.genfromtxt('iris_test.csv',delimiter=',',skip_header=1)
+x_test = dataIn[:,0:-1]
+y_test = keras.utils.to_categorical(dataIn[:,-1], num_classes=3)
+
+# Train
+model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=2000, batch_size=10)
+
+# Validate
+score = model.evaluate(featuresValid, one_hot_labelsV, batch_size=10)
+print(score)
+
+# Create a prediction set
+x_predict = np.array([[6.4, 3.2, 4.5, 1.5],
+                      [5.8, 3.1, 5.0, 1.7]], dtype=np.float32)
+y_predict = model.predict(x_predict)
+print(y_predict)
---
+++ ChallengeProblems/PossibleSolutions/KerasRegression.py
@@ -0,0 +1,53 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Create model
+model = Sequential()
+model.add(Dense(units=50, activation='relu', input_dim=2))
+model.add(Dense(units=130, activation='relu'))
+model.add(Dense(units=25, activation='relu'))
+model.add(Dense(units=1))
+
+model.compile(loss='mse',optimizer=keras.optimizers.Adagrad(lr=0.1))
+
+# Import data
+dataIn = np.genfromtxt('dataRegression_train.csv',delimiter=',')
+x_train = dataIn[:,0:2]
+y_train = dataIn[:,2]
+dataIn = np.genfromtxt('dataRegression_test.csv',delimiter=',')
+x_test = dataIn[:,0:2]
+y_test = dataIn[:,2]
+
+# Train
+model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=1000, batch_size=10)
+
+# Validate
+loss = model.evaluate(x_train, y_train, batch_size=10)
+print(loss)
+
+# Create a prediction set
+x_pred = np.mgrid[0:1:25j, 0:1:25j].reshape(2,-1).T
+y_pred = model.predict(x_pred)
+
+# Plot the actual and predicted values
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x1 = x_pred[:,0].reshape(25,-1)
+x2 = x_pred[:,1].reshape(25,-1)
+y = y_pred.reshape(25,-1)
+
+ax.scatter(x_train[:,0], x_train[:,1], y_train, c='r', marker='o')
+ax.scatter(x_test[:,0], x_test[:,1], y_test, c='b', marker='o')
+ax.plot_surface(x1,x2,y)
+
+ax.update({'xlabel':'x1', 'ylabel':'x2', 'zlabel':'f(x1,x2)'})
+
+plt.show()
