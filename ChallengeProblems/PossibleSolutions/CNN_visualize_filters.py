865cb738090ddede2b1f00f10a8b4d26c4d3d88a
Gregory Teichert <gregteichert@Gregorys-MacBook-Pro.local>
2018-10-23 11:59:02 -0400

    Separated the CNN filter visualizations into a separate file.

--- ChallengeProblems/PossibleSolutions/CNN_visualize_filters.py
+++ ChallengeProblems/PossibleSolutions/CNN_visualize_filters.py
@@ -5,6 +5,17 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, BatchNormalization
 
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+def plotFilters(W):
+    for i in range(8):
+        plt.subplot(4,2,i+1)
+        plt.imshow(W[:,:,0,i],interpolation="nearest",cmap=cm.bwr)
+
+    plt.draw()
+    plt.pause(0.001)
+
 # Load data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
@@ -15,13 +26,11 @@ y_test = keras.utils.to_categorical(y_test, num_classes=10)
 # Create model
 model = Sequential()
 model.add(BatchNormalization(input_shape=(28,28,1)))
-model.add(Conv2D(16,kernel_size=(5,5),
+model.add(Conv2D(8,kernel_size=(10,10),
                  activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(32,(5,5),activation='relu'))
-model.add(MaxPooling2D((2,2)))
 model.add(Flatten())
-model.add(Dense(500,activation='relu'))
+model.add(Dense(50,activation='relu'))
 model.add(Dropout(0.4))
 model.add(Dense(10,activation='softmax'))
 
@@ -30,4 +39,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
           
 # Train & plot filters
-model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=10, batch_size=100, shuffle=True)
+plotFilters(model.layers[1].get_weights()[0])
+for _ in range(10):
+    model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=1, batch_size=100, shuffle=True)
+    plotFilters(model.layers[1].get_weights()[0])
